b15256f31a4243b306e122d100ae6aae9ea8e85d
AntonTeslia <teslyaanton@yandex.ru>
2023-02-15 18:37:09 +0300

    feat: подключить БД

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,21 +1,33 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.service.HackCatService;
 
-@Controller
+import java.util.Optional;
+
+@RestController
 public class SimpleController {
-    // создаём логер
-    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
+    private final HackCatService hackCatService;
+
+    public SimpleController(HackCatService hackCatService) {
+        this.hackCatService = hackCatService;
+    }
+
+    // внедрите бин HackCatService
+
+    @GetMapping("/do-hack")
+    public Optional<String> doHack(){
+        // хакните этих котиков
+        return hackCatService.doHackNow()
+                .map(password -> "Ура! Пароль подобран: " + password)
+                .or(() -> Optional.of("Не удалось подобрать пароль. "
+                        + " Проверьте состояние и настройки базы данных."));
+    }
 
     @GetMapping("/home")
     public String homePage() {
-        // логируем факт получения запроса
-        log.info("Получен запрос.");
-
-        // возвращаем ответ в виде строки
         return "Котограм";
     }
 }
